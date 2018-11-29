854ed869405cf43e0aa2a25cc4fb588209d9d987
Chana <cgperel@gmail.com>
2018-11-28 18:43:58 -0600

    Finished practicing with class

--- SRC/ExceptionPractice.java
+++ SRC/ExceptionPractice.java
@@ -1,2 +1,31 @@
 public class ExceptionPractice {
+
+    private int threshold;;
+
+    public ExceptionPractice (int threshold){
+        this.threshold = threshold;
+    }
+
+    public int doTheThing (int value){
+        if(value < threshold){
+            throw new IllegalArgumentException(); //proper way to throw an exceptions
+        }
+        return value * 2;
+    }
+
+    public int doTheOtherThing(int value){
+        return value * 2;
+
+    }
+
+    public static void main(String[]args){
+        ExceptionPractice ep = new ExceptionPractice (0);
+        try {
+            int result = ep.doTheThing (-2);
+            System.out.println (result);//don't put this in try because won't show exception
+        } catch (IllegalArgumentException e) {//could also just write Exception but more specific is better- Exception will catch any problem, more specific will catch just this. E is almost always e- but could call it whatever you want
+            System.out.println ("I was expecting values greater than 0.");
+            e.printStackTrace ();
+        }
+    }
 }
